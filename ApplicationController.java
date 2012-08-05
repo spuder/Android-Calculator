d89521a0cfd9a53a138797d94f748002d7408068
spudstud <bubbleboy450@gmail.com>
2012-08-04 23:24:57 -0600

    Cosmetic & Readability Enhancements

--- ApplicationController.java
+++ ApplicationController.java
@@ -5,22 +5,31 @@ import java.util.HashMap;
 
 public class ApplicationController {
 	
-	
+	//Abstract Method, Notice how it doesn't have a body?
 	private static HashMap<String, Handler> controlHash = new HashMap<String, Handler>();
 	static{
 	String numKey = "numKey";
-//can we use reflection to set the types of these items in the hashmap so that we don't have to work so hard to get them
-	AddHandler theAddHandler = new AddHandler();
+
+	//When you want to run the code for addition, you call the hashmap with the key of '+',
+	//To add a new operation to the program, create a new class, add that class to the handler hashmap and done!
+	
+		  //Create a New Instance of the AdditionHandler and add it to the hashmap with key = '+'
+		  AddHandler theAddHandler = new AddHandler();
 	controlHash.put("+", theAddHandler);
-	SubHandler theSubHandler = new SubHandler();
+		//Create a New Instance of the SubHandler and add it to the hashmap with key = '-'
+		  SubHandler theSubHandler = new SubHandler();
 	controlHash.put("-", theSubHandler);
-	MultiHandler theMultiHandler = new MultiHandler();
+	
+		MultiHandler theMultiHandler = new MultiHandler();
 	controlHash.put("x", theMultiHandler);
-	DivideHandler theDivideHandler = new DivideHandler();
+	
+	       DivideHandler theDivideHandler = new DivideHandler();
 	controlHash.put("/", theDivideHandler);
-	EqualHandler theEqualHandler = new EqualHandler();
+	
+		EqualHandler theEqualHandler = new EqualHandler();
 	controlHash.put("=", theEqualHandler);
-	NumberValueHandler theNumValueHandler = new NumberValueHandler();
+	
+	     NumberValueHandler theNumValueHandler = new NumberValueHandler();
 	controlHash.put(numKey, theNumValueHandler);
 	
 	}	
@@ -29,7 +38,7 @@ public class ApplicationController {
 		//instead of setting to handler can we use reflection to set the Type of anOperation
 		//to the type of the result of controlHash.get(command); thereby giving us flexability
 		Handler anOperation = controlHash.get(command);
-		anOperation.handleIt(parameters);
+			anOperation.handleIt(parameters);
 	}
 
 }
